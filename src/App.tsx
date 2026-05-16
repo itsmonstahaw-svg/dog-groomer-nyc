94946c10c760be00ad1fe9f25d2e99a7f9448bfc
Ethan Williams <ethan.williams@imanage.com>
2026-05-16 17:14:17 +0100

    Remove BrowserRouter from App.tsx to fix SSR document error

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,4 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import ServicePetGrooming from "./pages/ServicePetGrooming";
@@ -25,15 +21,8 @@ import ClientSubmit from "./pages/ClientSubmit";
 import ThankYou from "./pages/ThankYou";
 
 
-const queryClient = new QueryClient();
-
 const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
+  <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/home" element={<Index />} />
           <Route path="/services/pet-grooming" element={<ServicePetGrooming />} />
@@ -57,9 +46,6 @@ const App = () => (
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
 );
 
 export default App;
